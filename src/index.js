4da3f4e9b3267cb18deb186c4a7774eadc19bc0f
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-06-25 19:02:19 -0700

    dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+const header = document.querySelector("header")
+
+const hamInput = document.querySelector("input")
+const hamLabel = document.querySelector("label")
+
+const nav = document.querySelector("nav")
+
+const menuTitle = document.querySelector("#sidebar-menu h3")
+const links = document.querySelectorAll("nav a")
+
+
+
 
