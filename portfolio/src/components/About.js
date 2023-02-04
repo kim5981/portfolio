2e2b64a1874818ed471776d0a037011c06ac5be0
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-04 15:41:30 -0800

    styles

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "../styles/About.css"
+import logo from "../logo.svg"
+import arrow from "../circle-down-solid.svg"
+
 
 export default function About(){
     return (
         <>
-        <section>
-            <h1>Hi! This is a greeting</h1>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <div> <button>Show more</button> <img alt="down arrow"/> </div>
+        <section className="about-wrap">
+            <div className="text">
 
-            <img alt="software engineer kimberly rodriguez"/>
+                <h1>Hi! This is a greeting</h1>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+
+                <div> 
+                    <button>Show more</button> 
+                    <arrow icon="fa-solid fa-circle-down"/>
+                </div>
+            </div>
+            
+            <img className="selfie" alt="software engineer kimberly rodriguez" src={ logo }/>
         </section>
         </>
     )
