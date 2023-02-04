448cc6bbaa4918f1b301dcbc41e0ca3c802a3f55
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-04 14:46:26 -0800

    rearranged styling files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Header/>
       <About/>
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
