9a3aca0f53f93fda391cdb8cd69463e4380388b3
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-04 14:32:59 -0800

    about basis

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About(){
+    return (
+        <>
+        <section>
+            <h1>Hi! This is a greeting</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div> <button>Show more</button> <img alt="down arrow"/> </div>
+
+            <img alt="software engineer kimberly rodriguez"/>
+        </section>
+        </>
+    )
+}
