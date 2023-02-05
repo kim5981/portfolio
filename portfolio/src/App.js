ea22740436b0d73f16cf42d35e6454d95d65beac
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-04 18:57:13 -0800

    footer started

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,22 +6,39 @@ import Tools from "./components/Tools"
 import Projects from './components/Projects';
 
 function App() {
+  const n = new Date()
   return (
     <div className="App">
+
       <Header/>
       <About/>
       <Tools/>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         
       <Projects/>  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div className='footer'>
+      <h2>Let's Connect!</h2>
+      <a
+            className="App-link"
+            href="https://www.linkedin.com/in/kimberlyrodriguez95/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img alt="Linked-In" src="#"/>
+          </a>
+
+          <a
+            className="App-link"
+            href="https://github.com/kim5981"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img alt="GitHub" src="#"/>
+          </a>
+          <p>Copyright { n.getFullYear() } All Rights Reserved</p>
+    </div>
+        
       
     </div>
   );
