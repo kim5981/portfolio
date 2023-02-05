86e91b7a6f04589ef7402563697da7fdbd6542fd
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-04 19:53:15 -0800

    footer framed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg'
 import './App.css';
 import Header from './components/header'
 import About from './components/About'
 import Tools from "./components/Tools"
 import Projects from './components/Projects';
+
+import github from "./github-logo.png"
+import linkedin from "./linkedin-logo.png"
+
 
 function App() {
   const n = new Date()
@@ -12,32 +15,42 @@ function App() {
 
       <Header/>
       <About/>
-      <Tools/>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        
+      <Tools/>  
       <Projects/>  
 
-    <div className='footer'>
+    <footer>
       <h2>Let's Connect!</h2>
-      <a
-            className="App-link"
-            href="https://www.linkedin.com/in/kimberlyrodriguez95/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img alt="Linked-In" src="#"/>
-          </a>
 
-          <a
-            className="App-link"
-            href="https://github.com/kim5981"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img alt="GitHub" src="#"/>
-          </a>
-          <p>Copyright { n.getFullYear() } All Rights Reserved</p>
-    </div>
+      <div className="links-wrap">
+
+      <a
+              href="https://www.linkedin.com/in/kimberlyrodriguez95/"
+              target="_blank"
+              rel="noopener noreferrer"
+        >
+              
+          <img className="link" src={ linkedin } alt="LinkedIn"/>
+            
+        </a>
+      
+     
+      
+        <a
+              href="https://github.com/kim5981"
+              target="_blank"
+              rel="noopener noreferrer"
+        >
+              
+          <img className="link" src={ github } alt="github"/>
+            
+        </a>
+      
+      </div>
+       
+      
+
+      <p>Copyright { n.getFullYear() } All Rights Reserved</p>
+    </footer>
         
       
     </div>
