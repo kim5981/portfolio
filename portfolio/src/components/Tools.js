6e74daa61c69e8c178993c2f11aded7a3b364147
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-04 16:37:37 -0800

    added tools icons and styled

--- portfolio/src/components/Tools.js
+++ portfolio/src/components/Tools.js
@@ -3,16 +3,34 @@ import "../styles/Tools.css"
 
 export default function Tools(){
     return (
-        <>
-        <section className="tools">
+        <section className="tools-wrap">
+            <h2>Tools & Technologies</h2>
+            <div className="tools">
+                <img alt="CSS" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+                {/* insert cypress icon */}
+                <img alt="Canva" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg" />
+                <img alt="Docker" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
+                <img alt="eslint" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eslint/eslint-original.svg" />
+                <img alt="Express" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" />
+                <img alt="Figma" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+                <img alt="git" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+                <img alt="HTML5" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" />
+                <img alt="Jest" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
+                <img alt="Jira" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg" />
+                <img alt="JavaScript" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+                <img alt="nodejs" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+                <img alt="npm" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
+                <img alt="Photoshop" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg" />
+                <img alt="Premiere Pro" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/premierepro/premierepro-plain.svg" />
+                <img alt="React" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                <img alt="Redux" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+                <img alt="Slack" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-plain.svg" />
+                <img alt="sqlite" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg" />
+                <img alt="VsCode" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
+                <img alt="Webpack" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-plain.svg" />
+                            
+            </div>
             
-        <img alt="CSS" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-        {/* insert cypress icon */}
-        <img alt="Canva" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg" />
-         
-        <img alt="Docker" className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
-           
         </section>
-        </>
     )
 }
