5c11391c0bd18faa27a50eab05c2564a6963cbd8
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-07 20:45:05 -0800

    project image onChange button coded. need to rework styling and reposition placeholders

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,16 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import "../styles/Projects.css"
+import project1 from "../project photos/friendly-login.png"
+import project1b from "../project photos/friendly-welcome.png"
 
 export default function Projects(){
+
+    const [ img, setImg ] = useState(project1)
+
+    function changeImage(){
+        if(img == project1){
+            setImg(project1b)
+        } else{
+            setImg(project1)
+        }
+    }
+
     return (
         <>
             <section className="project-wrap">
                 <h2>Projects</h2>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
+
                 <div className="projects">
-                    <img className="project" alt="placeholder" src="https://images.pexels.com/photos/196645/pexels-photo-196645.jpeg"/>
+
+                    <div className="carousel project" aria-label="carousel">
+                        <div className="slides fade" id="project-1a" aria-label="slide 1 of 2">
+                            <img className="slide-item" alt="social media project: friendly" src={ img }/>
+                        </div>
+
+                        <div id="dots">
+                            <span className="dot" onClick={ changeImage }></span>
+                            <span className="dot" onClick={ changeImage }></span>
+                        </div>
+                        
+                    </div>
+
+                    
+                    
+                      
+                                            
                     <img className="project" alt="placeholder" src="https://images.pexels.com/photos/326514/pexels-photo-326514.jpeg"/>
                     <img className="project" alt="placeholder" src="https://images.pexels.com/photos/247676/pexels-photo-247676.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
                     <img className="project" alt="placeholder" src="https://images.pexels.com/photos/6212801/pexels-photo-6212801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
