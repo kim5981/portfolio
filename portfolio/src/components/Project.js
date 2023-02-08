1243ba3a0c8df7b5e80e823946755c0fe632c648
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-07 21:12:54 -0800

    need to fix styling and placement for buttons

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../styles/Project.css"
 import project1 from "../project photos/friendly-login.png"
 import project1b from "../project photos/friendly-welcome.png"
 
@@ -6,6 +7,7 @@ import project1b from "../project photos/friendly-welcome.png"
 export default function Project (){
 
     const [ img, setImg ] = useState(project1)
+    const [ alt, setAlt ] = useState("a project by Kim Rodriguez")
 
     function changeImage(){
         if(img === project1){
@@ -15,16 +17,16 @@ export default function Project (){
         }
     }
     return(
-        <div className="carousel project" aria-label="carousel">
-                        <div className="slides fade" id="project-1" aria-label="slide 1 of 2">
-                            <img className="slide-item" alt="social media project: friendly" src={ img }/>
-                        </div>
+        <div className="project">
+            <div className="slides">
+                <img className="slide-item" alt={ alt } src={ img }/>
+            </div>
 
-                        <div id="dots">
-                            <span className="dot" onClick={ changeImage }></span>
-                            <span className="dot" onClick={ changeImage }></span>
-                        </div>
+            <div id="dots">
+                <span className="dot" onClick={ changeImage }></span>
+                <span className="dot" onClick={ changeImage }></span>
+            </div>
                         
-                    </div>
+        </div>
     )
 }
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import "../styles/Projects.css"
 
 import Project from "./Project"
